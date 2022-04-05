6d21b2c2fd33871e25363d1bde0f671d02a75ef1
Nadeem Butt <missedabit@users.noreply.github.com>
2022-04-05 10:32:05 +0100

    update

--- app/routes.js
+++ app/routes.js
@@ -3,18 +3,18 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 // Run this code when a form is submitted to 'juggling-balls-answer'
-router.post('/question1-answer', function (req, res) {
+router.post('/tasklistresponse/applicationdetails/aboutyou/aboutyou-answer', function (req, res) {
 
     // Make a variable and give it the value from 'how-many-balls'
-    var howMany = req.session.data['how-many']
+    var whereLive = req.session.data['detailsaboutyou']
   
     // Check whether the variable matches a condition
-    if (howMany == "ineligible"){
+    if (whereLive == "yes"){
       // Send user to next page
-      res.redirect('/ineligible')
+      res.redirect('/tasklistresponse/applicationdetails/aboutyou/c8/start')
     } else {
       // Send user to ineligible page
-      res.redirect('/question1')
+      res.redirect('/tasklistresponse/applicationdetails/aboutyou/aboutyou-no')
     }
   
   })
