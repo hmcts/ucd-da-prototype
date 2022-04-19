2b37b6e275197635848eb022b233f6f339360cd1
Nadeem Butt <missedabit@users.noreply.github.com>
2022-04-19 10:57:21 +0100

    update files

---
+++ app/assets/javascripts/childarrangements.js
@@ -0,0 +1,6 @@
+$(".childquestions1").hide();
+  
+$( ".addchild-button" ).on("click",function() {
+  alert( "Hello Marie" );
+  $(".childquestions1").show();
+});  
--- app/routes.js
+++ app/routes.js
@@ -2,21 +2,12 @@ const express = require('express')
 const router = express.Router()
 // Add your routes here - above the module.exports line
 // Run this code when a form is submitted to 'juggling-balls-answer'
-router.post('/tasklistresponse/applicationdetails/aboutyou/aboutyou-answer', function (req, res) {
-    // Make a variable and give it the value from 'how-many-balls'
-    var whereLive = req.session.data['detailsaboutyou']
-    // Check whether the variable matches a condition
-    if (whereLive == "yes"){
-      // Send user to next page
-      res.redirect('/tasklistresponse/applicationdetails/aboutyou/c8/start')
-    } else {
-      // Send user to ineligible page
-      res.redirect('/tasklistresponse/applicationdetails/aboutyou/c8/personaldetails')
-    }
-  });
+
+
   router.post('/tasklistresponse/safetyconcerns/riskofharm/childrenabuse-answer', function (req, res) {
       // Make a variable and give it the value from 'how-many-balls'
       var childAbuse = req.session.data['abuse']
+
       // Check whether the variable matches a condition
       if (childAbuse == "yes"){
         // Send user to next page
@@ -25,10 +16,13 @@ router.post('/tasklistresponse/applicationdetails/aboutyou/aboutyou-answer', fun
         // Send user to ineligible page
         res.redirect('/tasklistresponse/start')
       }
+
     });
+
     router.post('/tasklistresponse/safetyconcerns/riskofharm/start-answer', function (req, res) {
         // Make a variable and give it the value from 'how-many-balls'
         var respAbuse = req.session.data['abuse']
+
         // Check whether the variable matches a condition
         if (respAbuse == "yes"){
           // Send user to next page
@@ -37,10 +31,13 @@ router.post('/tasklistresponse/applicationdetails/aboutyou/aboutyou-answer', fun
           // Send user to ineligible page
           res.redirect('/tasklistresponse/safetyconcerns/riskofharm/childrenabuse')
         }
+
       });
+
       router.post('/tasklistresponse/safetyconcerns/riskofharm/abduction-answer', function (req, res) {
           // Make a variable and give it the value from 'how-many-balls'
           var childAbd = req.session.data['abduction']
+
           // Check whether the variable matches a condition
           if (childAbd == "yes"){
             // Send user to next page
@@ -49,10 +46,13 @@ router.post('/tasklistresponse/applicationdetails/aboutyou/aboutyou-answer', fun
             // Send user to ineligible page
             res.redirect('/tasklistresponse/safetyconcerns/riskofharm/drugs')
           }
+
         });
+
         router.post('/tasklistresponse/safetyconcerns/riskofharm/threats-answer', function (req, res) {
             // Make a variable and give it the value from 'how-many-balls'
             var abdThreat = req.session.data['threat']
+
             // Check whether the variable matches a condition
             if (abdThreat == "yes"){
               // Send user to next page
@@ -61,10 +61,13 @@ router.post('/tasklistresponse/applicationdetails/aboutyou/aboutyou-answer', fun
               // Send user to ineligible page
               res.redirect('/tasklistresponse/safetyconcerns/riskofharm/childlocation')
             }
+
           });
+
         router.post('/tasklistresponse/safetyconcerns/riskofharm/passportoffice-answer', function (req, res) {
             // Make a variable and give it the value from 'how-many-balls'
             var passport = req.session.data['passport']
+
             // Check whether the variable matches a condition
             if (passport == "yes"){
               // Send user to next page
@@ -73,10 +76,13 @@ router.post('/tasklistresponse/applicationdetails/aboutyou/aboutyou-answer', fun
               // Send user to ineligible page
               res.redirect('/tasklistresponse/safetyconcerns/riskofharm/drugs')
             }
+
           });
+
           router.post('/tasklistresponse/applicationdetails/proceedings/start-answer', function (req, res) {
               // Make a variable and give it the value from 'how-many-balls'
               var proceedings = req.session.data['answer']
+
               // Check whether the variable matches a condition
               if (proceedings == "yes"){
                 // Send user to next page
@@ -85,5 +91,7 @@ router.post('/tasklistresponse/applicationdetails/aboutyou/aboutyou-answer', fun
                 // Send user to ineligible page
                 res.redirect('/tasklistresponse/start')
               }
+
             })
+
 module.exports = router
