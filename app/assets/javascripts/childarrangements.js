352fd50f4c8b9bd293b9fade489dd7bd0dc6d829
Nadeem Butt <missedabit@users.noreply.github.com>
2022-05-03 19:02:17 +0100

    add remove js

--- app/assets/javascripts/childarrangements.js
+++ app/assets/javascripts/childarrangements.js
@@ -8,3 +8,7 @@ $(".upload-addnew").on('click', function(){
   var newel = $('.uploadcomponent:last').clone();
   $(newel).insertAfter(".uploadcomponent:last");
 });
+
+$('body').on('click','.remove-button', function() {
+    $(this).closest('.uploadcomponent').remove();
+});
