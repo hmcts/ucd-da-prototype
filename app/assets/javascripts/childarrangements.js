5c75b84bbcdf8f26b4ce5eb3a270ffd6c117a862
Nadeem Butt <missedabit@users.noreply.github.com>
2022-05-03 12:42:34 +0100

    upload pages

--- app/assets/javascripts/childarrangements.js
+++ app/assets/javascripts/childarrangements.js
@@ -1,6 +1,15 @@
 $(".childquestions1").hide();
   
 $( ".addchild-button" ).on("click",function() {
-  alert( "Hello Marie" );
   $(".childquestions1").show();
 });  
+
+$(".upload-addnew").on('click', function(){
+
+  // Create clone of <div class='input-form'>
+  var newel = $('.uploadcomponent:last').clone();
+  // Add after last <div class='input-form'>
+  $(newel).insertAfter(".uploadcomponent:last");
+
+
+});
